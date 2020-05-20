5284f99350590f9b934a4767471a6938ade8c8f2
Kaival <kaivalpatel53@gmail.com>
2020-05-20 11:30:03 +0530

    eleventh commit

--- controllers/product.js
+++ controllers/product.js
@@ -36,7 +36,8 @@ exports.createProduct = (req,res) => {
             call fields.name, fields.price etc blah blah
             */
             const {name,description,price,category,stock} = fields;
-
+            console.log(fields);
+            console.log(file);
             //if any of the field is not filled then we gonna throw error.
             if(!name || !description || !price || !category || !stock){
                 return res.status(400).json({
@@ -55,6 +56,7 @@ exports.createProduct = (req,res) => {
                 }
                 product.photo.data = fs.readFileSync(file.photo.path);
                 product.photo.contentType = file.photo.type
+                console.log(product);
             }
             //save to db
             product.save(
@@ -65,8 +67,177 @@ exports.createProduct = (req,res) => {
                        })
                     }
                     res.json(product);
+                    console.log(product);
                 }
             )
+            
         }
     )
 }
+
+exports.getProduct = (req,res) =>{
+    req.product.photo = undefined
+    return res.json(req.product);
+    /*doing this is good , but when it comes to mp3,pdf,pictures which are bulky in size
+    cannot be served in get request 
+    return res.json(req.product)
+    */
+    
+}
+
+
+//middleware to parse photo alongwith loading the getProduct for quick response
+exports.photo = (req,res,next) =>{
+    if(req.product.photo.data){
+        res.set("Content-Type",req.product.photo.contentType)
+        return res.send(req.product.photo.data)
+    }
+    next();
+
+}
+
+
+//delete controller
+exports.deleteProduct = (req,res) => {
+    let product = req.product;
+    product.remove(
+        (err,deletedProduct) => {
+            if(err){
+                return res.status(400).json({
+                    error:"Failed to delete this Product",
+                })
+            }
+            res.json({
+                message:"Successfully Deleted this Product",
+                deletedProduct,
+            });
+        }
+    );
+}
+
+//update Controller
+exports.updateProduct = (req,res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(
+        req,(err,fields,file)=>{
+            if(err){
+                res.status(400).json({
+                    error:"Problem with form processing!"
+                });
+            }
+            //destructure the fields
+            /*=>it is equal to fields.price, fields.name, etc. So everytime we dont want to 
+            call fields.name, fields.price etc blah blah
+            */
+            const {name,description,price,category,stock} = fields;
+            console.log(fields);
+            console.log(file);
+            //if any of the field is not filled then we gonna throw error.
+            if(!name || !description || !price || !category || !stock){
+                return res.status(400).json({
+                    error:"Please fill out all the fields!"
+                });
+            }   
+            
+            let product = req.product;
+
+            //fields are populated(updated in product) by this lodash method
+            product = _.extend(product, fields)
+
+            //handle file
+            if(file.photo){
+                if(file.photo.size > 3145728){
+                    return res.status(400).json({
+                        error:"File Size too Big!"
+                    })
+                }
+                product.photo.data = fs.readFileSync(file.photo.path);
+                product.photo.contentType = file.photo.type
+                
+            }
+            //save to db
+            product.save(
+                (err,product)=>{
+                    if(err){
+                       res.status(400).json({
+                        error:"Error Updating Product!",
+                       })
+                    }
+                    res.json(product);
+                    console.log(product);
+                }
+            )
+            
+        }
+    )
+}
+
+//product Listing
+exports.getAllProducts =(req,res) => {
+    //default product show limit in one page is 8 if user doesn't pass
+    //by default it will give the limit as string so parsing to int
+    let showlimit = req.query.limit ? parseInt(req.query.limit) : 8;
+    //taking sorting parameter
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    Product.find().
+    select("-photo").
+    //populating category
+    populate("category").
+    //sorting by ascending order
+    sort([[sortBy, "asc"]]).
+    limit(showlimit).
+    exec(
+        (err,products) => {
+            if(err){
+                res.status(400).json({
+                    error:"Failed to get the Products"
+                });
+            }
+            res.json(products)
+        }
+    )
+};
+
+//middleware to update the stock
+//LOADING FKIN WEIRD SYNTAX
+exports.updateStock = (req,res,next) => {
+    let myOperations = req.body.order.products.map(prod => {
+            return {
+                updateOne : {
+                    filter : {_id:prod._id},
+                    update : {$inc : {stock: -prod.count, sold: +prod.count}}
+                } 
+            }
+        }
+    );
+    Product.bulkWrite(myOperations,
+        {}, 
+        (err,products) => {
+            if(err){
+                return res.status(400).json({
+                    error:"Bulk Operation Failed"
+                });
+            }
+            next();
+        }
+    )
+}
+
+
+//getting all the categories of the product
+exports.getAllUniqueCategories = (req,res) => {
+    Product.distinct(
+        "category",
+        {},
+        (err,categories) => {
+            if(err){
+                res.status(400).json({
+                    error:"Cannot Load All the Categories"
+                })
+            }
+            res.json(categories);
+        }
+    )
+}
+
--- routes/product.js
+++ routes/product.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router();
 const {isSignedIn,isAdmin,isAuthenticated} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
-const {getProductById,createProduct} = require("../controllers/product")
+const {getProductById,createProduct,getProduct,photo,deleteProduct,updateProduct,getAllProducts,getAllUniqueCategories} = require("../controllers/product")
 
 
 // all of params
@@ -10,8 +10,25 @@ router.param("userId",getUserById);
 router.param("productId",getProductById);
 
 // all of actual routes
+
+//read routes
 router.post("/product/create/:userId",isSignedIn,isAuthenticated,isAdmin,createProduct);
+router.get("/product/:productId",getProduct)
+router.get("/product/photo/:productId",photo)
 
 
+//delete routes
+router.delete("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,deleteProduct);
+
+
+//update routes
+router.put("/product/:productId/:userId",isSignedIn,isAuthenticated,isAdmin,updateProduct);
+
+
+//listing routes
+router.get("/products",getAllProducts);
+
+//listing categories
+router.get("/products/categories",getAllUniqueCategories);
 
 module.exports = router;
